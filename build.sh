61da217639f4b66805faeb940278e513eac63afc
Esha kurmi <kurmiesha2002@gmail.com>
2026-05-26 20:50:56 +0530

    Fix

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 pip install --upgrade pip
-pip install fastapi uvicorn python-dotenv pydantic pypdf
-pip install langchain-core langchain-text-splitters
-pip install langchain langchain-community
-pip install langchain-groq langchain-huggingface
-pip install langchain-pinecone pinecone-client --no-cache-dir
-pip install sentence-transformers
-pip install pinecone
-pip install pyyaml
-pip install gunicorn
+pip install -r requirements.txt
