4cb2c3c171729613c164a5d2f5c8028b7d3e4882
Esha kurmi <kurmiesha2002@gmail.com>
2026-05-26 18:02:36 +0530

    Add build.sh content

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+pip install --upgrade pip
+pip install fastapi uvicorn python-dotenv pydantic pypdf
+pip install langchain-core langchain-text-splitters
+pip install langchain langchain-community
+pip install langchain-groq langchain-huggingface
+pip install langchain-pinecone
+pip install sentence-transformers
+pip install pinecone
+pip install pyyaml
