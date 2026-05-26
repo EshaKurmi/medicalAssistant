d63276190ce198fefaebd13067a4741af1e0a7fa
Esha kurmi <kurmiesha2002@gmail.com>
2026-05-26 21:30:11 +0530

    fix embiddings and logger

--- server/rag/embeddings.py
+++ server/rag/embeddings.py
@@ -8,7 +8,9 @@ import os
 def get_embedding_function():
     logger.info("Loading embedding model...")
     return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="sentence-transformers/paraphrase-MiniLM-L3-v2",
+        model_kwargs={"device": "cpu"},
+        encode_kwargs={"batch_size": 8}
     )
 
 def get_pinecone_client():
@@ -18,33 +20,27 @@ def get_pinecone_client():
 
 def build_vector_store():
     logger.info("Building Pinecone vector store...")
-
     docs = load_documents()
-
     if not docs:
         logger.error("Koi document nahi mila!")
         raise ValueError("No documents found in medical_docs folder")
-
     embedding = get_embedding_function()
     pc, index_name = get_pinecone_client()
-
     existing = [i.name for i in pc.list_indexes()]
     if index_name not in existing:
-        logger.info(f"Index bana raha hoon...")
+        logger.info("Index bana raha hoon...")
         pc.create_index(
             name=index_name,
             dimension=384,
             metric="cosine",
             spec=ServerlessSpec(cloud="aws", region="us-east-1")
         )
-
     logger.info("Documents Pinecone mein upload ho rahe hain...")
     db = PineconeVectorStore.from_documents(
         documents=docs,
         embedding=embedding,
         index_name=index_name
     )
-
     logger.info("Pinecone vector store ready!")
     return db
 
@@ -52,7 +48,6 @@ def load_vector_store():
     logger.info("Pinecone vector store load ho raha hai...")
     embedding = get_embedding_function()
     _, index_name = get_pinecone_client()
-
     return PineconeVectorStore(
         index_name=index_name,
         embedding=embedding
