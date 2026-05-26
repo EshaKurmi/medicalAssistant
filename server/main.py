2dba59ef5382024d69de6b1edd9f927fbfd7357f
Esha kurmi <kurmiesha2002@gmail.com>
2026-05-26 22:59:38 +0530

    remove startup

--- server/main.py
+++ server/main.py
@@ -1,23 +1,11 @@
 from fastapi import FastAPI
-import logging
 
 app = FastAPI()
-
-logger = logging.getLogger(__name__)
 
 
 @app.on_event("startup")
 async def startup_event():
-    try:
-        logger.info("Application started")
-
-        # IMPORTANT:
-        # startup pe heavy indexing mat chalao
-
-        logger.info("Skipping vector build")
-
-    except Exception as e:
-        logger.error(str(e))
+    print("Server started")
 
 
 @app.get("/")
